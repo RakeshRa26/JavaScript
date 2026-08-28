3e7bafe81de4559f2f4af7433d0a95046139b5bf
Rakesh Rawal <107682542+RakeshRa26@users.noreply.github.com>
2026-08-28 14:23:02 +0000

    DataTypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)   // We are using node.js, not browser
+
+console.log(3+3);  // code readability should b
+
+console.log("Rakesh Rawal");
+
+
+let name = "Rak"
+let age = 25
+let isloggedIn = false;
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol 
+
+
+// object
+
+console.log(typeof null);   // object
+console.log(typeof undefined)   // undefined
