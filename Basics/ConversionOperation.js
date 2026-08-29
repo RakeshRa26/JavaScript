755f9afd472486b52759a3284020e95c35b6688a
Rakesh Rawal <107682542+RakeshRa26@users.noreply.github.com>
2026-08-29 12:00:22 +0000

    Update Code

--- Basics/ConversionOperation.js
+++ Basics/ConversionOperation.js
@@ -32,4 +32,42 @@ let score = "33ab"
  console.log(typeof stringNumber);
  
 
- 
+ // *************************************** Operations ****************************************
+
+
+ let value = 3
+ let negValue = -value
+// console.log(negValue);
+
+console.log(2+2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
+console.log(2-2)
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 =str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2+ "2");
+
+// console.log((3+4) * 5 % 3);
+
+
+
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+
+
+
