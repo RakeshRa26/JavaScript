aa8ec32d5cd8a271b85ae3f21534d8304d95f389
Rakesh Rawal <107682542+RakeshRa26@users.noreply.github.com>
2026-08-29 10:02:36 +0000

    Conversion of Datatype

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,35 @@
+
+let score = "33ab"
+
+ console.log(typeof score);
+ console.log(typeof (score));
+ 
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+ // "33" => 33
+ // "33ab" => NaN
+ // true => 1; false => 0
+
+ let isloggedIn = 1;
+
+ let boooleanIsLoggedIn = Boolean(isloggedIn)
+ console.log(boooleanIsLoggedIn);
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "Rakesh" => true
+
+
+ let someNumber = "33"
+
+ let stringNumber = String(someNumber)
+
+ console.log(stringNumber)
+ console.log(typeof stringNumber);
+ 
+
+ 
