771543c6e511155041b7327946b132f3fe70bb73
Rakesh Rawal <107682542+RakeshRa26@users.noreply.github.com>
2026-09-01 15:37:47 +0000

    Dates

---
+++ Basics/DatesInJs.js
@@ -0,0 +1,40 @@
+// Dates
+
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+// let myCreateDate = new Date(2026,0,12)
+// let myCreateDate = new Date(2026,0,12, 5, 3)
+// let myCreateDate = new Date("2026-00-19")
+let myCreateDate = new Date("01-14-2026")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.getTime());
+
+
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
+
+
+
